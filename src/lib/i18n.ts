0716ec73e233afe5d1aa93b0b9c84cf855d74c17
Jorin <jorin.s@protonmail.com>
2025-10-24 13:30:45 +0200

    Remove unused

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,6 +1,5 @@
 import { readFileSync } from 'node:fs'
 
-import defaultAllergens from '../content/l10n/sv/allergens.json'
 import defaultAlt from '../content/l10n/sv/alt.json'
 import defaultMeta from '../content/l10n/sv/meta.json'
 import defaultUi from '../content/l10n/sv/ui.json'
@@ -39,7 +38,6 @@ export function setUrlLocale(url: URL, lang: string): string {
  * that yet to be set values can be used pre-emptively.
  */
 type LocalisationValues = {
-  allergens: typeof defaultAllergens & Record<string, string | undefined>
   alt: typeof defaultAlt & Record<string, string | undefined>
   meta: typeof defaultMeta & Record<string, string | undefined>
   ui: typeof defaultUi & Record<string, string | undefined>
@@ -49,7 +47,6 @@ type LocalisationValues = {
  * The default localisation values, currently hard-coded to be Swedish.
  */
 const defaultLocalisationValues: LocalisationValues = {
-  allergens: defaultAllergens,
   alt: defaultAlt,
   meta: defaultMeta,
   ui: defaultUi,
@@ -66,19 +63,16 @@ export class Localisation {
   values: LocalisationValues;
 
   constructor(lang: LanguageCode) {
-    const localisedAllergensText = readFileSync(`./src/content/l10n/${lang}/allergens.json`)
     const localisedAltText = readFileSync(`./src/content/l10n/${lang}/alt.json`)
     const localisedMetaText = readFileSync(`./src/content/l10n/${lang}/meta.json`)
     const localisedUiText = readFileSync(`./src/content/l10n/${lang}/ui.json`)
 
-    const localisedAllergens = JSON.parse(localisedAllergensText.toString())
     const localisedAlt = JSON.parse(localisedAltText.toString())
     const localisedMeta = JSON.parse(localisedMetaText.toString())
     const localisedUi = JSON.parse(localisedUiText.toString())
 
     this.lang = lang
     this.values = {
-      allergens: localisedAllergens,
       alt: localisedAlt,
       meta: localisedMeta,
       ui: localisedUi,
